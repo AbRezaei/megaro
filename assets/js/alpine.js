63ad52954ceb28edb3459126f57cf297d63bb74b
abrezaei <arezaei620@gmail.com>
2026-02-14 17:15:14 +0100

    index page - check availability

--- assets/js/alpine.js
+++ assets/js/alpine.js
@@ -2,6 +2,9 @@ import Alpine from "alpinejs";
 import collapse from "@alpinejs/collapse";
 import pageStore from "./widgets/pageStore.js";
 import dropdown from "./widgets/dropdown.js";
+import tabs from "./widgets/tabs.js";
+import select from "./widgets/select.js";
+import datepicker from "./widgets/datepicker.js";
 
 // Alpine init
 document.addEventListener('alpine:init', () => {
@@ -10,6 +13,9 @@ document.addEventListener('alpine:init', () => {
 
     // Alpine data
     Alpine.data('dropdown', dropdown);
+    Alpine.data('tabs', tabs);
+    Alpine.data('select', select);
+    Alpine.data('datepicker', datepicker);
 });
 
 Alpine.plugin(collapse);
